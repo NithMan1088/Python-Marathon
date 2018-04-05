e29e94a7862de2a10fe8389f94de4ecafdaca317
NithMan1088 <manish2008kathait@gmail.com>
2018-04-05 18:24:23 -0400

    Update Strings-II.py

--- Strings-II.py
+++ Strings-II.py
@@ -4,9 +4,8 @@
 # In[3]:
 
 
-#Given 2 strings, a and b, return a string of the form short+long+short,
-#with the shorter string on the outside and the longer string on the inside.
-#The strings will not be the same length, but they may be empty (length 0).
+#Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer
+#string on the inside. The strings will not be the same length, but they may be empty (length 0).
 
 
 def combo_string(a,b):
